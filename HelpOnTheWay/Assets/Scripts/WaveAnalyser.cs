e1e9be4fbb4fe1d04173453cefd718bbe76767d4
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2017-09-28 11:01:45 -0300

    change

---
+++ HelpOnTheWay/Assets/Scripts/WaveAnalyser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+namespace Assets.Scripts
+{
+    class WaveAnalyser
+    {
+        AudioSource Source;
+
+
+        public void Initialize(AudioSource src)
+        {
+            Source = src;
+
+        }
+        public float GetFrequency(float freq)
+        {
+            if (Source.isPlaying)
+            {
+                float[] spectrum = new float[512];
+                Source.GetSpectrumData(spectrum, 0, FFTWindow.Hamming);
+
+                //float interval = Source.clip;
+
+                return spectrum[(int)(freq * P)];
+               
+            }
+            else
+            {
+                Debug.Log("not playing");
+                return 0;
+            }
+        }
+    }
+}
--- HelpOnTheWay/Assets/Scripts/WaveEmitter.cs
+++ HelpOnTheWay/Assets/Scripts/WaveEmitter.cs
@@ -2,49 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Assets.Scripts;
 public class WaveEmitter : MonoBehaviour
 {
     public AudioClip clip;
     public AudioClip clip2;
 
-    public Text analysis;
+    public Text textRef;
     public AudioSource source;
     public Text CurrentSample;
+
+
+    WaveAnalyser Analyser;
 
     private void Awake()
     {
         clip.LoadAudioData();
         clip2.LoadAudioData();
+        Analyser = new WaveAnalyser();
     }
     void Start ()
     {
         source = gameObject.AddComponent<AudioSource>();
         source.clip = clip;
+        Analyser.Initialize(source);
         source.Play();
 	}
     float previousMedian = 0;
 	void Update ()
     {
-        if (source.isPlaying)
+        float a = Analyser.GetFrequency(440.0f);
         {
-            float[] spectrum = new float[256];
-            source.GetSpectrumData(spectrum,0,FFTWindow.Rectangular);
-            float median = 0;
-            for (int i = 0; i < 256; i++)
-            {
-                median += spectrum[i];
-            }
-            if (median / spectrum.Length > previousMedian)
-            {
-                previousMedian = median / spectrum.Length;
-                Append(previousMedian.ToString());
-            }
-            CurrentSample.text = (median / spectrum.Length).ToString();
+            Append(a.ToString());
         }
 	}
     void Append(string toAppend)
     {
-        analysis.text += toAppend + " \n";
+        textRef.text += toAppend + " \n";
     }
    public void ChangeCoiso()
     {
